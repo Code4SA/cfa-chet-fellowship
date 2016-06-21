fca6253bf127c584e6c0fe1e1c8ae3d02bfa5ec1
Guus Hoekman <guushoekman@gmail.com>
2016-06-21 13:47:34 +0200

    works now?

--- infographics/embed.js
+++ infographics/embed.js
@@ -1,1 +1,10 @@
-$.get('http://code4sa.org/cfa-chet-fellowship/infographics/body.html', function(html) { $('.region-footer').prepend($.parseHTML(html, true)); })
+$.getScript("http://code4sa.org/cfa-chet-fellowship/infographics/assets/js/bootstrap.min.js").done( function( ) {
+	console.log("bootstrap");
+    $.getScript("http://code4sa.org/cfa-chet-fellowship/infographics/assets/js/cfamerica-lib.min.js").done( function( ) {
+    	console.log("code4america");
+		$.get('body.html', function(html) {
+			$('.region-footer').prepend($.parseHTML(html, true));
+  			console.log("the rest");
+        });
+    });
+});
