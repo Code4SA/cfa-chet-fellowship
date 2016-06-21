e12e0308ed3b2a081e1b33780aa5722244dfcb1d
Guus Hoekman <guushoekman@gmail.com>
2016-06-21 14:00:16 +0200

    individual getscripts

--- infographics/embed.js
+++ infographics/embed.js
@@ -1,10 +1,19 @@
 $.getScript("http://code4sa.org/cfa-chet-fellowship/infographics/assets/js/bootstrap.min.js").done( function( ) {
 	console.log("bootstrap");
     $.getScript("http://code4sa.org/cfa-chet-fellowship/infographics/assets/js/cfamerica-lib.min.js").done( function( ) {
-    	console.log("code4america");
-		$.get('http://code4sa.org/cfa-chet-fellowship/infographics/body.html', function(html) {
-			$('.region-footer').prepend($.parseHTML(html, true));
-  			console.log("the rest");
+    console.log("code4america");
+	    $.getScript("http://code4sa.org/cfa-chet-fellowship/infographics/assets/js/clipboard.min.js").done( function( ) {
+	    console.log("clipboard");
+		    $.getScript("http://code4sa.org/cfa-chet-fellowship/infographics/assets/js/exportexcel.js").done( function( ) {
+		    console.log("exportexcel");
+				$.getScript("http://code4sa.org/cfa-chet-fellowship/infographics/assets/js/lightbox.min.js").done( function( ) {
+			    console.log("lightbox");
+				    $.get('http://code4sa.org/cfa-chet-fellowship/infographics/body.html', function(html) {
+					$('.region-footer').prepend($.parseHTML(html, true));
+	  				console.log("body");
+			    	});	  				
+			    });
+		    });
         });
     });
 });
