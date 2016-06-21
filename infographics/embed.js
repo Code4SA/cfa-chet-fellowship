de116d1430a9a88196e8e4db27676e93c7d42f8d
Guus Hoekman <guushoekman@gmail.com>
2016-06-21 13:49:44 +0200

    url fix

--- infographics/embed.js
+++ infographics/embed.js
@@ -2,7 +2,7 @@ $.getScript("http://code4sa.org/cfa-chet-fellowship/infographics/assets/js/boots
 	console.log("bootstrap");
     $.getScript("http://code4sa.org/cfa-chet-fellowship/infographics/assets/js/cfamerica-lib.min.js").done( function( ) {
     	console.log("code4america");
-		$.get('body.html', function(html) {
+		$.get('http://code4sa.org/cfa-chet-fellowship/infographics/body.html', function(html) {
 			$('.region-footer').prepend($.parseHTML(html, true));
   			console.log("the rest");
         });
